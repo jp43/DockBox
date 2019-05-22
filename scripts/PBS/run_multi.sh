1fa8e4738cc87c4f79f3e1a3eb9c74d32efe6b28
Ubuntu <ubuntu@ip-172-31-37-173.eu-west-1.compute.internal>
2019-05-22 14:08:54 +0000

    created example folders

--- scripts/PBS/run_multi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# submit jobs
-for dir in lig{1..8}; do
-  cd $dir
-  qsub ../run.pbs # submit job
-  cd ..
-done
--- scripts/SLURM/run_multi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# submit jobs
-for dir in lig{1..8}; do
-  cd $dir
-  sbatch ../run.slurm # submit job
-  cd ..
-done
